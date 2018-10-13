2160a2d63aa841658c1a02ef58fe3e4ddc497a28
zhuyijing <zhuyijing168@163.com>
2018-10-13 17:21:19 +0800

    refine

--- alpaca.py
+++ alpaca.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
+from os import listdir
+from os.path import isfile, join
 
-imgs = ['4871539221530_.pic.jpg', '4881539221531_.pic.jpg']
+imgpath = './img/'
+
+imgs = [f for f in listdir(imgpath) if isfile(join(imgpath, f))]
+
+# imgs = ['4871539221530_.pic.jpg', '4881539221531_.pic.jpg']
 
 for img in imgs:
     print('<img src="img/%s" alt="hi" class="inline"/>\n' % img)
+
+print('<img src="qrcode.jpg" alt="hi" class="inline"/>\n')
